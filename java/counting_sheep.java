cafd999f7e9bef0cd69f91350b741ad5fd331079
stoic-plus <ricardoledesmadev@gmail.com>
2019-04-13 11:29:09 -0600

    adds java

---
+++ java/counting_sheep.java
@@ -0,0 +1,25 @@
+// Consider an array of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+//
+// For example,
+//
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
+//
+// Hint: Don't forget to check for bad values like null/undefined
+
+public class Counter {
+  public int countSheeps(Boolean[] arrayOfSheeps) {
+    int count = 0;
+    for (Boolean x:arrayOfSheeps) {
+      if (x != null && x) {
+        count++;
+      }
+    }
+    return count;
+  }
+}
